94c6175848fda5d27cfaa5079ae47c02b5b9bbad
MohamedAhmed315 <mohamedahmed315@hotmail.com>
2021-01-13 21:21:25 +0200

    Background Color Selector

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let body = document.querySelector("#body1");
+
+let setGradient = function () {
+    body.style.background = "linear-gradient(to right , " + color1.value + ", " + color2.value + ")";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+console.log(color1);
+console.log(color2);
